5a8c86b9fa485eca1ed6961e79181c78cb89fe52
Daniel Alvarez <ingdanielf8@gmail.com>
2024-06-19 17:34:19 -0500

    Add pigmodule to the app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CustomersModule } from './modules/customers/customers.module';
 import { IsObjectId } from './common/validators/is-object-id.validator';
 import { PaymentsModule } from './modules/payments/payments.module';
+import { PigsModule } from './modules/pigs/pigs.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1/agro_admin_v1'),
     CustomersModule,
-    PaymentsModule
+    PaymentsModule,
+    PigsModule,
   ],
   controllers: [],
   providers: [IsObjectId],
