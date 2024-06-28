8e1a7a44c8a06edc9e2b94b341b4252e6d137c86
Daniel Alvarez <ingdanielf8@gmail.com>
2024-06-28 11:49:40 -0500

    Added env system  adn CORS
    Check out the Docs!
    obsidian://open?vault=Agro-Admin-Back&file=CORS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env.local', override: true });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }));
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
 }
 bootstrap();
