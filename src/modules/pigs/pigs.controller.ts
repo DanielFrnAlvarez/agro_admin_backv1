2ed39b423ad53bed83a6fd9dc4c211d8e2e94285
Daniel Alvarez <ingdanielf8@gmail.com>
2024-07-16 21:34:39 -0500

    Update pigs.controller.ts

--- src/modules/pigs/pigs.controller.ts
+++ src/modules/pigs/pigs.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { PigsService } from './pigs.service';
 import { CreatePigDto } from './dto/create-pig.dto';
+import { ValidatePigStatePipe } from './pipes/pigs/validate-pig-state.pipe';
 
 @Controller('Pigs')
 export class PigsController {
   constructor(private readonly pigsService: PigsService) {}
 
   @Post()
+  @UsePipes(ValidatePigStatePipe)
   async createPig(@Body() createPigDto: CreatePigDto) {
     return this.pigsService.createPig(createPigDto);
   }
